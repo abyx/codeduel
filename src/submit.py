46dbb1db2fb18bce573ff8584c362f37cb0d3ab6
Ran Tavory <rantav@gmail.com>
2011-05-31 19:28:05 +0300

    Use a template here

--- src/submit.py
+++ src/submit.py
@@ -1,6 +1,14 @@
+import os
+from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 
 class SubmitPage(webapp.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Please submit!')
+  def get(self):
+    template_values = {
+        'greetings': 'hello',
+        'url': 'http://www.google.com',
+        'url_linktext': 'google',
+        }
+
+    path = os.path.join(os.path.dirname(__file__), 'submit.html')
+    self.response.out.write(template.render(path, template_values))
