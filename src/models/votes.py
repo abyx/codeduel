6e76176e6bd85d76dd5af1cba45cd6df4f255484
Avihu Turzion <avihut@il.ibm.com>
2011-05-31 19:55:34 +0300

    merged all models into one file

--- src/models/votes.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import db
-from snippet import Snippet
-
-class Vote(db.Model):
-    winner = db.ReferenceProperty(Snippet)
-    loser = db.ReferenceProperty(Snippet)
-    created_at = db.DateTimeProperty(auto_add_now=True)
-    
