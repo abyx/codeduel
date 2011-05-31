df57b97874424af6e518dc8e127cc66cc4525586
Yoni Tsafir <tsafir@il.ibm.com>
2011-05-31 20:40:10 +0300

    Implemented snippet page

--- src/snippet.py
+++ src/snippet.py
@@ -1,14 +1,16 @@
 import os
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
+from src.models import Snippet
 
 class SnippetPage(webapp.RequestHandler):
-  def get(self):
-    template_values = {
-        'greetings': 'hello',
-        'url': 'http://www.google.com',
-        'url_linktext': 'google',
-        }
+    def get(self):
+        template_values = {
+                           'snippet': self._get_snippet(),
+                           }
 
-    path = os.path.join(os.path.dirname(__file__), 'snippet.html')
-    self.response.out.write(template.render(path, template_values))
+        path = os.path.join(os.path.dirname(__file__), 'snippet.html')
+        self.response.out.write(template.render(path, template_values))
+
+    def _get_snippet(self):
+        return Snippet.get(self.request.get("id"))
